a01dbb516663c58a975f48e2021c6cb5868770ce
Wang Yi <yi.wang-ext@ikang.com>
2023-03-17 18:32:55 +0800

    feat: 数组排序手写

---
+++ write-practise-js/6.sort.js
@@ -0,0 +1,30 @@
+const arr = [9, 4, 1, 6, 8, 2, 7, 5, 5, 6, 3, 9]
+// 冒泡排序：双层循环比较排序，内循环每一次循环比较相邻值，进行换位
+function bubbleSort (arr) {
+  const len = arr.length
+  for (let i = 0; i < len; i++) {
+    let flag = false
+    for (let j = 0; j < len - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+        flag = true
+      }
+    }
+    if (!flag) return
+  }
+}
+bubbleSort(arr)
+
+// 选择排序：双层循环比较排序，外循环每一次循环指定最小值的下标，内循环的每一次循环比较最小值
+function selectSort (arr) {
+  const len = arr.length
+  for (let i = 0; i < len; i++) {
+    let minIndex = i
+    for (let j = 1; j < len; j++) {
+      if (arr[j] < arr[minIndex]) minIndex = j
+      if (minIndex !== i) [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]]
+    }
+    if (!flag) return
+  }
+}
+bubbleSort(arr)
