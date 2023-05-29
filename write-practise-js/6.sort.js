0543b8912fb0663edf1acbe275205ce6b40020ce
Wang Yi <yi.wang-ext@ikang.com>
2023-05-29 15:35:28 +0800

    feat: 插入排序、快排、归并

--- write-practise-js/6.sort.js
+++ write-practise-js/6.sort.js
@@ -27,3 +27,63 @@ function selectSort (arr) {
   }
 }
 selectSort(arr)
+
+// 插入排序，双层循环，如果外层循环判断出需要换位，那么就需要在内层循环将换位后之前的数据都比较一边
+function insertSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let j = i;
+    let target = arr[j];
+    while (j > 0 && arr[j - 1] > target) {
+      arr[j] = arr[j - 1];
+      j--;
+    }
+    arr[j] = target;
+  }
+}
+insertSort(arr)
+
+// 快排，核心是递归，指定一个值通过递归分组，直到最后分出长度为1然后返回
+function quickSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  const cur = arr[arr.length - 1];
+  const left = arr.filter((v, i) => v <= cur && i !== arr.length - 1);
+  const right = arr.filter((v) => v > cur);
+  return [...quickSort(left), cur, ...quickSort(right)];
+}
+quickSort([3, 6, 2, 4, 1])
+
+// 归并
+function merge(left, right) {
+  let res = [];
+  let i = 0;
+  let j = 0;
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      res.push(left[i]);
+      i++;
+    } else {
+      res.push(right[j]);
+      j++;
+    }
+  }
+  if (i < left.length) {
+    res.push(...left.slice(i));
+  } else {
+    res.push(...right.slice(j));
+  }
+  return res;
+}
+
+function mergeSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  const mid = Math.floor(arr.length / 2);
+
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
+mergeSort(arr)
